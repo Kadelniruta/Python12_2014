85ac20d8bc1a4c22035722814578b734eb31448b
Kadelniruta <kadelniruta@gmail.com>
2024-01-21 17:32:35 +0545

    update

--- main.py
+++ main.py
@@ -1,3 +1,64 @@
-print("Hello World")
-print("Hello",35,"hi")
+# print("Hello World")
+# print("Hello",35,"hi")
+
+# What is variable?
+# A variable is a user-defined name stored in the memory of computer.
+
+# x=10
+# print(10)
+# name ="niru"
+# age=20
+# print(name, age)
+# print("My name is", name)
+# concat
+# print("My name is"+ name)
+# print(f"my name is{name} age{age}")
+
+
+# name=input("Enter user name: ")
+# print(name)
+
+
+# name = input("Enter your name: ")
+# email = input("Enter your email: ")
+# address = input("Enter your address: ")
+# phone = input("Enter your phone number: ")
+
+# print(f"My name is {name}, My email is {email}, My name is {address}, My phone number is {phone}")
+
+# name, age = input("Enter name and age: ".split(","))
+# print(name)
+# print(age)
+
+
+#WAP to enter five subject marks and calculate total and percentage
+
+# Math = input(f"Enter the marks of Math: ")
+# Datamining = input(f"Enter the marks of Datamining")
+# Python = input(f"Enter the marks of Python: ")
+# English = input(f"Enter the marks of English: ")
+# Physics = input(f"Enter the marks of Physics: ")
+# total_marks = int(Math)+int(Datamining)+int(Python)+int(English) +int(Physics)
+# percentage = (total_marks/500)*100
+# print(f"Total Marks are : {total_marks}")
+# print(f"Percentage are : {percentage}")
+
+
+#WAP to enter P,T,R and calculate simple interest
+# P = float(input('Enter Principal amount: '))
+# T = float(input('Enter Time period: '))
+# R = float(input('Enter Rate of Interest: '))
+# SI = (P*T*R)/100
+# print(f"Simple Interest is ", {SI})
+
+
+T = float(input('Enter Time period: '))
+R = float(input('Enter Rate of Interest: '))
+SI = float(input("Enter Simple Interest:"))
+P = (SI/(T*R))
+print(f'Principal Amount is ',{P})
+
+
+
+
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+Math = input(f"Enter the marks of Math: ")
+# Datamining = input(f"Enter the marks of Datamining")
+# Python = input(f"Enter the marks of Python: ")
+# English = input(f"Enter the marks of English: ")
+# Physics = input(f"Enter the marks of Physics: ")
+# total_marks = int(Math)+int(Datamining)+int(Python)+int(English) +int(Physics)
+# percentage = (total_marks/500)*100
+# print(f"Total Marks are : {total_marks}")
+# print(f"Percentage are : {percentage}")
